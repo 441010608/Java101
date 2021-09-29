9e967bbd8e142632f7d4ac6730b36ede8157c1aa
Hani M. Th <hmthubaiti@uqu.edu.sa>
2021-09-30 00:09:43 +0300

    fix the only issue

--- Assignment1.java
+++ Assignment1.java
@@ -3,8 +3,9 @@ import java.util.Scanner;
 
 //muhmmed jafar 
 //s441010608
-
-
+//your grade is 4.5/5
+//you are amazing, keep it up
+//@ change the class name from Main to match the file name Assignment1 ( golden rule )
 class Main {
   public static void main(String[] args) {
     Scanner myNum = new Scanner(System.in);
