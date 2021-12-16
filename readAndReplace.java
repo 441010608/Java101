f1501402b76ac6360cf0120a23aeb61fe7a435a9
441010608 <91159726+441010608@users.noreply.github.com>
2021-12-16 07:46:08 +0300

    Create readAndReplace.java

---
+++ readAndReplace.java
@@ -0,0 +1,15 @@
+public class ReadAndReplace{
+public static void readAndReplace(String[] args) {
+     String name = "muhammed jafar almhdawi";
+     for (int x =0;  x<=20 ; ++x){
+         if(name.charAt(x) == 'm'){
+        System.out.print('x');}
+        else {
+            System.out.print(name.charAt(x));
+        }
+        
+     }
+    
+  }
+    
+ }
