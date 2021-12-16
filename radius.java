f871895fa029bb310172ffa992476e07af455da4
441010608 <91159726+441010608@users.noreply.github.com>
2021-12-16 08:00:19 +0300

    Create radius.java

---
+++ radius.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Radius 
+{
+	public static void radius(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter your Number");	
+	double var1 = scan.nextDouble();
+	System.out.println(3.14*(Math.pow(var1, 2)));
+	}
+}
+
