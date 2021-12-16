f59f244c0a8ba26c2a55193377a7e7a30fb8af95
441010608 <91159726+441010608@users.noreply.github.com>
2021-12-16 07:53:27 +0300

    Create age.java

---
+++ age.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Age
+{
+	public static void age(String[] args) {
+		Scanner scan = new Scanner(System.in);
+	    System.out.println("Enter age ");
+		int num = scan.nextInt();
+		System.out.println(2021 - num );
+	}
+}
